d619e4ac143e4a4d6282db9e1d4d177eddacf935
David Boedigheimer <stealth259@gmail.com>
2021-05-25 16:00:04 -0700

    Add migrations of seed data.

---
+++ TravelAPI/Migrations/20210525224819_userName.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAPI.Migrations
+{
+    public partial class userName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Review",
+                table: "Locations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Locations",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Locations",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "User_Name",
+                table: "Locations",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "User_Name",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Review",
+                table: "Locations",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Locations",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Locations",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Locations",
+                nullable: true);
+        }
+    }
+}
---
+++ TravelAPI/Migrations/20210525225817_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "LocationId", "City", "Country", "Rating", "Review", "User_Name" },
+                values: new object[,]
+                {
+                    { 1, "Rorcek", "Sweden", 7, "Wonderful and sublime. Fantastic atmostphere and amazing people.", "Malinda" },
+                    { 2, "England", "America", 2, "Terrible, this is not the england I was promised.", "Charlie" },
+                    { 3, "Topnick", "Ireland", 6, "Cold and rainy, but fantasic food and drinks!", "Derik" },
+                    { 4, "Tokyo", "Japan", 9, "An eastern version of New York City!", "Amy" },
+                    { 5, "YorkTown", "England", 3, "Don't know what I was expexting, but it wasn't this.", "Bob" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 5);
+        }
+    }
+}
