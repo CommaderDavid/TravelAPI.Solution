8e4252b0ea2fad6e8d95d93869041b3707997052
David Boedigheimer <stealth259@gmail.com>
2021-07-01 14:09:17 -0700

    Add update to database>

---
+++ TravelAPI/Migrations/20210701210450_ReName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAPI.Migrations
+{
+    public partial class ReName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "User_Name",
+                table: "Reviews",
+                newName: "UserName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Reviews",
+                newName: "User_Name");
+        }
+    }
+}
